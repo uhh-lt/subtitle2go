58eaef38c023b14fbe2475fd7988c7daba6ff0ab
Alienmaster <mitachundkrach@gmail.com>
2020-08-31 15:37:50 +0200

    first prototype

---
+++ download_models.sh
@@ -0,0 +1,5 @@
+mkdir models/
+cd models/
+mv ../kaldi_tuda_de_nnet3_chain2.yaml ./
+wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
+tar xvfj de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
---
+++ nnet3-recognizer.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from kaldi.asr import NnetLatticeFasterRecognizer, LatticeLmRescorer
+from kaldi.decoder import LatticeFasterDecoderOptions
+from kaldi.fstext import SymbolTable, shortestpath, indices_to_symbols
+from kaldi.fstext.utils import get_linear_symbol_sequence
+from kaldi.nnet3 import NnetSimpleComputationOptions
+from kaldi.util.table import SequentialMatrixReader
+import yaml
+
+# yaml 
+config_file = "kaldi_tuda_de_nnet3_chain2.yaml"
+with open(config_file, 'r') as stream:
+    model_yaml = yaml.safe_load(stream)
+decoder_yaml_opts = model_yaml['decoder']
+# print (decoder_yaml_opts)
+
+
+# Construct recognizer
+decoder_opts = LatticeFasterDecoderOptions()
+decoder_opts.beam = 13
+decoder_opts.max_active = 7000
+decodable_opts = NnetSimpleComputationOptions()
+decodable_opts.acoustic_scale = 1.0
+decodable_opts.frame_subsampling_factor = 3
+decodable_opts.frames_per_chunk = 150
+# hier yaml Datei einfügen
+asr = NnetLatticeFasterRecognizer.from_files(
+    "final.mdl", "HCLG.fst", "words.txt",
+    decoder_opts=decoder_opts, decodable_opts=decodable_opts)
+
+
+# Construct symbol table
+symbols = SymbolTable.read_text("words.txt")
+phi_label = symbols.find_index("#0")
+
+# # Construct LM rescorer
+# rescorer = LatticeLmRescorer.from_files("G.fst", "G.rescore.fst", phi_label)
+
+# Define feature pipelines as Kaldi rspecifiers
+feats_rspec = "ark:compute-mfcc-feats --config=conf/mfcc_hires.conf scp:wav.scp ark:- |"
+ivectors_rspec = (
+    "ark:compute-mfcc-feats --config=conf/mfcc_hires.conf scp:wav.scp ark:-"
+    " | ivector-extract-online2 --config=ivector.conf ark:spk2utt ark:- ark:- |"
+    )
+# Decode wav files
+with SequentialMatrixReader(feats_rspec) as f, \
+     SequentialMatrixReader(ivectors_rspec) as i:
+    for (fkey, feats), (ikey, ivectors) in zip(f, i):
+        assert(fkey == ikey)
+        out = asr.decode((feats, ivectors))
+        words, _, _ = get_linear_symbol_sequence(shortestpath(out["lattice"]))
+        print(fkey, " ".join(indices_to_symbols(symbols, words)), flush=True)
