d230b306643f3f1c35a0619e694b38c91ffacd32
bmilde <bmilde@M>
2022-02-28 14:26:00 +0100

    added time stamps

--- event_server.py
+++ event_server.py
@@ -5,6 +5,7 @@ import argparse
 import threading
 import flask
 import redis
+import json
 
 from werkzeug.serving import WSGIRequestHandler
 from flask import jsonify,json,Response
@@ -23,15 +24,19 @@ current_jobs = {}
 
 def persistence_event_stream():
     global current_jobs
-    print("Estabilishing persistence event_stream...")
+    print('Estabilishing persistence event_stream...')
     pubsub = red.pubsub()
     pubsub.subscribe(redis_server_channel)
     for message in pubsub.listen():
         msg = str(message['data'])
         print('New msg:', msg)
+        if 'pid' in msg:
+            msg_json = json.loads(msg)
+            key = str(msg_json['pid']) + '_' + msg_json['file_id']
+            current_jobs[key] = msg_json
 
 def event_stream():
-    print("New connection to event_stream!")
+    print('New connection to event_stream!')
     pubsub = red.pubsub()
     pubsub.subscribe(reids_server_channel)
  #   yield b'hello'
@@ -41,10 +46,25 @@ def event_stream():
             #print(type(message['data']))
             yield b'data: %s\n\n' % message['data']
 
+@app.route('/status')
+def status():
+    return jsonify(current_jobs)
+
+@app.route('/clear')
+def clear_finished():
+    to_delete = []
+    for key in current_jobs:
+        if 'finished' in current_jobs[key]['status'] \
+          or 'failed' in current_jobs[key]['status'] :
+            to_delete.append(key)
+    for key in to_delete:
+        del current_jobs[key]
+    return 'ok'
+
 #Event stream end point for the browser, connection is left open. Must be used with threaded Flask.
 @app.route('/stream')
 def stream():
-    return flask.Response(event_stream(), mimetype="text/event-stream")
+    return flask.Response(event_stream(), mimetype='text/event-stream')
 
 #Traditional long polling. This is the fall back, if a browser does not support server side events. 
 @app.route('/stream_poll')
@@ -74,5 +94,5 @@ if __name__ == '__main__':
     print('Host:', args.host)
     print('Port:', args.port)
 
-    WSGIRequestHandler.protocol_version = "HTTP/1.1"
+    WSGIRequestHandler.protocol_version = 'HTTP/1.1'
     app.run(host=args.host, port=args.port, threaded=True, use_reloader=False, use_debugger=False)
