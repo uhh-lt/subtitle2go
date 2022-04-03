e220d7db893b4688e69a7dfb20172ec8dae09998
bmilde <bmilde@M>
2022-04-03 23:59:32 +0200

    added new de model

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,8 @@
 mkdir models/
 cd models/
-mv ../kaldi_tuda_de_nnet3_chain2.yaml ./
-wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
+mv ../kaldi_tuda_de_nnet3_chain2.yaml ../kaldi_tuda_de_nnet3_chain2_de_722k.yaml ./
+#wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
+wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_722k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
 wget http://ltdata1.informatik.uni-hamburg.de/subtitle2go/Model_subs_norm1_filt_5M_tageschau_euparl_h256_lr0.02.pcl
-tar xvfj de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
+tar xvfj de_722k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
+#tar xvfj de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
