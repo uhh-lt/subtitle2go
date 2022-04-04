2fd1c926c76ce83803781db057bea9186bd79b90
bmilde <bmilde@M>
2022-04-04 19:25:52 +0200

    typo

--- download_models.sh
+++ download_models.sh
@@ -7,11 +7,11 @@ cd models/
 #tar xvfj de_683k_nnet3chain_tdnn1f_2048_sp_bi_smaller_fst.tar.bz2
 
 wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_722k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
-wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_772k_G.carpa.bz2
-wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_772k_rnnlm_lstm_2x.tar.bz2
+wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_722k_G.carpa.bz2
+wget http://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_722k_rnnlm_lstm_2x.tar.bz2
 wget http://ltdata1.informatik.uni-hamburg.de/subtitle2go/Model_subs_norm1_filt_5M_tageschau_euparl_h256_lr0.02.pcl
 tar xvfj de_722k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
-bunzip2 de_772k_G.carpa.bz2
-tar xvfj de_772k_rnnlm_lstm_2x.tar.bz2
+bunzip2 de_722k_G.carpa.bz2
+tar xvfj de_722k_rnnlm_lstm_2x.tar.bz2
 
 cd ..
