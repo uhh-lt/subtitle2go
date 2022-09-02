54b0d300cfa5506ead36e8464e70d26a41d6b8cd
bmilde <bmilde@gmail.com>
2022-09-02 15:49:03 +0200

    added rnnlm download to download script

--- download_models.sh
+++ download_models.sh
@@ -19,11 +19,15 @@ wget https://ltdata1.informatik.uni-hamburg.de/subtitle2go/Model_subs_norm1_filt
 # en models
 wget https://ltdata1.informatik.uni-hamburg.de/pykaldi/en_160k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
 
+wget https://ltdata1.informatik.uni-hamburg.de/kaldi_tuda_de/de_900k_rnnlm_lstm_4x.tar.bz2
+
 tar xvfj de_900k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
 bunzip2 de_900k_G.carpa.bz2
 tar xvfj de_900k_rnnlm_lstm_4x.tar.bz2
 
 tar xvfj en_160k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
+
+tar xvfj de_900k_rnnlm_lstm_4x.tar.bz2
 
 wget http://ltdata1.informatik.uni-hamburg.de/subtitle2go/interpunct_de_rpunct.tar.gz
 tar xfvz interpunct_de_rpunct.tar.gz
