5b202315a694cfbc6365f84b0550c06e1237b0cf
mttsh <matthias.hitzler@uni-hamburg.de>
2022-11-03 09:49:40 +0100

    Fix model download

--- download_models.sh
+++ download_models.sh
@@ -19,9 +19,9 @@ bunzip2 de_900k_G.carpa.bz2
 tar xvfj de_900k_rnnlm_lstm_4x.tar.bz2
 
 # en kaldi models:
-wget https://ltdata1.informatik.uni-hamburg.de/pykaldi/en_160k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
+wget https://ltdata1.informatik.uni-hamburg.de/subtitle2go/en_200k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
 
-tar xvfj en_160k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
+tar xvfj en_200k_nnet3chain_tdnn1f_2048_sp_bi.tar.bz2
 
 # de punctuation models:
 # wget https://ltdata1.informatik.uni-hamburg.de/subtitle2go/Model_subs_norm1_filt_5M_tageschau_euparl_h256_lr0.02.pcl
@@ -30,7 +30,8 @@ tar xfvz interpunct_de_rpunct.tar.gz
 
 
 # en punctuation models:
+mkdir interpunct_en_rpunct
 wget http://ltdata1.informatik.uni-hamburg.de/subtitle2go/interpunct_en_rpunct.tar.gz
-tar xfvz interpunct_en_rpunct.tar.gz
+tar xfvz interpunct_en_rpunct.tar.gz --directory interpunct_en_rpunct/
 
 cd ..
